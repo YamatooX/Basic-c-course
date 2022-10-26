69c23a72206d88b6fa88d5395e3658cad7d242a2
YamatooX <jarbatorek@gmail.com>
2022-10-26 13:41:20 +0200

    Handling classes, pbejcts and namespaces

---
+++ Homework2-More objects/Classes/Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework2_More_objects.Classes.Class2
+{
+    class Class2
+    {
+    }
+}
---
+++ Homework2-More objects/Program.cs
@@ -0,0 +1,23 @@
+﻿using Homework2_More_objects.Classes;
+using Homework2_More_objects.Classes.Class2;
+using System;
+
+namespace Homework2_More_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Class1 class11 = new Class1();
+
+            Class2 class2 = new Class2();
+            Class2 class22 = new Class2();
+
+            ClassesInDifferentNamepspace.Class1 classFromOtherNamespace = new ClassesInDifferentNamepspace.Class1();
+            var classFromOtherNamespace2 = new ClassesInDifferentNamepspace.Class1();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
