d2ffc3d3d1d5cc7dea6355a4ed610934e27d10c0
YamatooX <jarbatorek@gmail.com>
2022-10-26 15:54:09 +0200

    If statements

---
+++ Homework4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace Homework4
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            User user = new User(33);
+            User user2 = new User(999);
+        }
+    }
+}
