b42f95a19d4ab91fd43a34343fd589745c055721
YamatooX <jarbatorek@gmail.com>
2022-10-27 11:45:58 +0200

    Ifs

---
+++ Homework4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace Homework4
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            User user = new User(33);
+            User user2 = new User(999);
+
+            int[] ints = new int[54];
+            User[] users = new User[5454];
+        }
+    }
+}
---
+++ Homework4/User.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Homework4
+{
+    public class User
+    {
+        public string Name;
+        public double Money;
+        public UserType userType;
+
+        public User(double money)
+        {
+            Name = "Jarek";
+            Money = money;
+            printInfoAboutUser();
+        }
+
+        public User() { }
+
+        public void PrintInfoAboutUser()
+        {
+            NumberFormatInfo nfi = new CultureInfo("en-US", false).NumberFormat;
+
+            double number = Money;
+            double priceAfterDiscount;
+
+            if (number > 300)
+            {
+                priceAfterDiscount = number * 0.9;
+                Console.WriteLine($"You have 10% discount => {priceAfterDiscount.ToString("N", nfi)}");
+                if (priceAfterDiscount > 400)
+                {
+                    userType = UserType.Premium;
+                }
+            }
+            else if (number > 200)
+            {
+                priceAfterDiscount = number * 0.85;
+                Console.WriteLine($"You have 15% discount => {priceAfterDiscount.ToString("N", nfi)}");
+                userType = UserType.Standard;
+            }
+            else
+            {
+                priceAfterDiscount = number * 0.80;
+                Console.WriteLine($"You have 20% discount => {priceAfterDiscount.ToString("N", nfi)}");
+                userType = UserType.Standard;
+            }
+            Console.WriteLine($"Thank you for your money {Name} {userType}");
+        }
+    }
+    public enum UserType
+    {
+        Premium,
+        Standard
+    }
+}
